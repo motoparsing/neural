85aeeb51b5396eef4dd51821f7b31f6b57827f9e
Дима <motoparsing>
2018-04-02 05:40:35 +0300

    nachalo

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+class Neuron {
+public:
+	string name;
+	int	input[29][29];
+	int output;
+	int memory[29][29];
+};
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Neuron neuroWeb[32];
+	for(int i = 0; i <= 32; i++) {
+		Neuron neuroWeb[i];
+		neuroWeb.output = 0;
+		int asciiVal = 192 + i;
+		char asciiChar = asciiVal;
+		neuroWeb.name = asciiChar;
+		cout << neuroWeb << endl;
+
+	}
+
+	system("PAUSE");
+}
